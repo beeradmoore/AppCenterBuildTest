7aef2a05306fe0f8bfe0fe42ceb1890961248c6c
Brad Moore <brad@4pi.com.au>
2021-08-26 17:35:20 +1000

    Added pre-build script.

---
+++ AppCenterBuildTest.Android/appcenter-pre-build.sh
@@ -0,0 +1,8 @@
+﻿#!/usr/bin/env bash
+# App Center custom build scripts: https://aka.ms/docs/build/custom/scripts
+
+export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
+
+dotnet tool install --global boots --version 1.0.4.624
+boots --stable Mono
+boots --stable XamarinAndroid
